fcd61de7de53fbef3d54d867923868ad35f82e01
tongjiyiming <tongjiyiming@gmail.com>
2018-09-03 16:02:11 -0400

    change EOF setting to only use LF ending. set run_docker.sh to include running option for jupyterlab. add jupyterlab extension TableOfContent

---
+++ cuda-tensorflow/gpu_test_tf.py
@@ -0,0 +1,49 @@
+import edward as ed
+import tensorflow as tf
+import numpy as np
+
+# Creates a graph.
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
+
+# run a Bayesion Linear Regression model with edward
+def build_toy_dataset(N, w, noise_std=0.1):
+  D = len(w)
+  x = np.random.randn(N, D)
+  y = np.dot(x, w) + np.random.normal(0, noise_std, size=N)
+  return x, y
+
+N = 40  # number of data points
+D = 10  # number of features
+
+w_true = np.random.randn(D)
+X_train, y_train = build_toy_dataset(N, w_true)
+X_test, y_test = build_toy_dataset(N, w_true)
+
+from edward.models import Normal
+
+X = tf.placeholder(tf.float32, [N, D])
+w = Normal(loc=tf.zeros(D), scale=tf.ones(D))
+b = Normal(loc=tf.zeros(1), scale=tf.ones(1))
+y = Normal(loc=ed.dot(X, w) + b, scale=tf.ones(N))
+
+qw = Normal(loc=tf.get_variable("qw/loc", [D]),
+            scale=tf.nn.softplus(tf.get_variable("qw/scale", [D])))
+qb = Normal(loc=tf.get_variable("qb/loc", [1]),
+            scale=tf.nn.softplus(tf.get_variable("qb/scale", [1])))
+
+inference = ed.KLqp({w: qw, b: qb}, data={X: X_train, y: y_train})
+inference.run(n_samples=5, n_iter=250)
+
+y_post = ed.copy(y, {w: qw, b: qb})
+
+print("Mean squared error on test data:")
+print(ed.evaluate('mean_squared_error', data={X: X_test, y_post: y_test}))
+
+print("Mean absolute error on test data:")
+print(ed.evaluate('mean_absolute_error', data={X: X_test, y_post: y_test}))
---
+++ cuda-tensorflow/jupyter_notebook_config.py
@@ -0,0 +1,52 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+from jupyter_core.paths import jupyter_data_dir
+import subprocess
+import os
+import errno
+import stat
+from subprocess import call
+
+decrypted = call(["openssl", "req", "-x509", "-nodes", "-days", "365", "-subj", "/C=US/ST=VA", "-newkey", "rsa:1024", "-keyout", "/home/lem/.jupyter/mykey.key", "-out", "/home/lem/.jupyter/mycert.pem"])
+print(decrypted)
+
+### test for theano gpu running
+decrypted = call(["python", "/home/lem/gpu_test_tf.py"])
+print(decrypted)
+##################
+
+c = get_config()
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.password='sha1:559f89535f2d:c320212e6708222a537f4f1d3efada91c1a8c5ca' \
+
+c.NotebookApp.keyfile = '/home/lem/.jupyter/mykey.key'
+c.NotebookApp.certfile = '/home/lem/.jupyter/mycert.pem'
+
+# https://github.com/jupyter/notebook/issues/3130
+c.FileContentsManager.delete_to_trash = False
+
+# Generate a self-signed certificate
+if 'GEN_CERT' in os.environ:
+    dir_name = jupyter_data_dir()
+    pem_file = os.path.join(dir_name, 'notebook.pem')
+    try:
+        os.makedirs(dir_name)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(dir_name):
+            pass
+        else:
+            raise
+    # Generate a certificate if one doesn't exist on disk
+    subprocess.check_call(['openssl', 'req', '-new',
+                           '-newkey', 'rsa:2048',
+                           '-days', '365',
+                           '-nodes', '-x509',
+                           '-subj', '/C=XX/ST=XX/L=XX/O=generated/CN=generated',
+                           '-keyout', pem_file,
+                           '-out', pem_file])
+    # Restrict access to the file
+    os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
+    c.NotebookApp.certfile = pem_file
